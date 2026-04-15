087f648e0aa38445506b2e57a83b2cab49a2d32c
srGida <150749214+srGida@users.noreply.github.com>
2026-04-14 22:01:44 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import TruuGoApp from './App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <TruuGoApp />
-  </React.StrictMode>
-);
